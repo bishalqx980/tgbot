4895eb13b56ecbcb1af59157928c3038f7aabdd1
Bishal Hasan Bhuiyan <94838617+bishalqx980@users.noreply.github.com>
2024-01-04 18:43:20 +0600

    minor fix

--- bot/ytdl.py
+++ bot/ytdl.py
@@ -3,23 +3,26 @@ from pytube import YouTube
 
 class YouTubeDownload:
     def ytdl(url):
-        yt = YouTube(url)
-        title = yt.title
-        file_type = "audio"
-        extention = "mp3"
-        order_by = "abr"
-        file_path = "ytdl/download/"
+        try:
+            yt = YouTube(url)
+            title = yt.title
+            file_type = "audio"
+            extention = "mp3"
+            order_by = "abr"
+            file_path = "ytdl/download/"
 
-        stream = (
-            yt.streams
-            .filter(type=file_type)
-            .order_by(order_by)
-            .desc()
-            .first()
-        )
-        if stream:
-            filename = f"{title}.{extention}"
-            file_path = stream.download(output_path=file_path, filename=filename)
-            return title, file_path
-        else:
-            print("No stream found for this video")
+            stream = (
+                yt.streams
+                .filter(type=file_type)
+                .order_by(order_by)
+                .desc()
+                .first()
+            )
+            if stream:
+                filename = f"{title}.{extention}"
+                file_path = stream.download(output_path=file_path, filename=filename)
+                return title, file_path
+            else:
+                print("No stream found for this video")
+        except Exception as e:
+            print(f"Error ytdl: {e}")
