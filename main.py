e15eca04707c50292093f413aa5e6c6f47d31d29
Bishal Hasan Bhuiyan <94838617+bishalqx980@users.noreply.github.com>
2024-01-05 12:41:10 +0600

    ytdl exception added

--- main.py
+++ main.py
@@ -412,9 +412,13 @@ async def func_ytdl(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if url != "":
             tmp_msg = await Message.reply_msg(update, "Please Wait...")
             await Message.edit_msg(update, "📥 Downloading...", tmp_msg)
-            res = await YouTubeDownload.ytdl(url)
+            try:
+                res = await YouTubeDownload.ytdl(url)
+            except Exception as e:
+                print(f"Error ytdl: {e}")
+                await Message.reply_msg(update, f"Error: {e}")
             if res:
-                await Message.edit_msg(update, "📥 Uploading...", tmp_msg)
+                await Message.edit_msg(update, "📤 Uploading...", tmp_msg)
                 try_attempt = 0
                 max_attempt = 3
                 while try_attempt <= max_attempt:
@@ -436,8 +440,6 @@ async def func_ytdl(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     await Message.del_msg(chat.id, tmp_msg)
                 except Exception as e:
                     print(f"Error os.remove: {e}")
-            else:
-                await Message.edit_msg(update, f"Something Went Wrong! Try again after sometime...", tmp_msg)
         else:
             await Message.reply_msg(update, "Use <code>/ytdl youtube_url</code> to download a video!")
     else:
