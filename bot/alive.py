eb7ff441f9adc00c74a029a2cfe97cacf64bb27f
bishalqx980 <bishalqx680@gmail.com>
2024-06-07 23:50:51 +0600

    Heroku port binding issue fixed

--- bot/alive.py
+++ bot/alive.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template
+import os
 from threading import Thread
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
+PORT = int(os.environ.get("PORT", 8080))
 
 @app.route('/')
 def index():
   return render_template("index.html")
 
-
 def run():
-  app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=PORT)
 
 def alive():
   Thread(target=run).start()
