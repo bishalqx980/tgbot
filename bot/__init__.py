71f19fa286a2f76e9e56fd5db2c75286c9f1a49b
Bishal Hasan Bhuiyan <bishalqx680@gmail.com>
2025-04-19 13:03:52 +0600

    Version: 1.1.1.491 (patch)

--- bot/__init__.py
+++ bot/__init__.py
@@ -6,7 +6,7 @@ from .config import CONFIG
 from .logger import setup_logging
 
 # constants
-__version__ = "1.1.0.490" # major.minor.patch.commits
+__version__ = "1.1.1.491" # major.minor.patch.commits
 CONFIG_FILE = "config.env"
 REQUIRED_DIRS = ["downloads", "sys"]
 ORIGINAL_BOT_USERNAME = "MissCiri_bot"
