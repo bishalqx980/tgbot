b75a573698316d13ee1267444d92189dff63fe4b
Bishal Hasan Bhuiyan <bishalqx680@gmail.com>
2025-06-11 12:53:46 +0600

    HotFix

--- bot/__init__.py
+++ bot/__init__.py
@@ -6,7 +6,7 @@ from .utils.config import CONFIG
 from .utils.logger import setup_logging
 
 # constants
-__version__ = "1.4.0.514" # major.minor.patch.commits
+__version__ = "1.4.1.515" # major.minor.patch.commits
 CONFIG_FILE = "config.env"
 REQUIRED_DIRS = ["downloads", "sys"]
 ORIGINAL_BOT_USERNAME = "MissCiri_bot"
