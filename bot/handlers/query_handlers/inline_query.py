3ce6a4943e19e9a93191e43026da452c9cdb1c09
Bishal Hasan Bhuiyan <bishalqx680@gmail.com>
2025-06-18 14:32:57 +0600

    Updated: inlineQuery

--- bot/handlers/query_handlers/inline_query.py
+++ bot/handlers/query_handlers/inline_query.py
@@ -1,36 +1,56 @@
 from uuid import uuid4
 from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import ContextTypes
+from bot import logger
+from bot.helpers import BuildKeyboard
 from bot.modules.base64 import BASE64
+
+def inlineQueryMaker(title, message, reply_markup=None, description=None):
+    """
+    :param description: Type `same` if you want to keep the description same as message!
+    """
+    if not reply_markup:
+        reply_markup = BuildKeyboard.cbutton([{"Try inline": "switch_to_inline"}])
+    
+    try:
+        content = InlineQueryResultArticle(
+            str(uuid4()),
+            title=title,
+            input_message_content=InputTextMessageContent(message),
+            reply_markup=reply_markup,
+            description=message if description == "same" else description
+        )
+
+        return content
+    except Exception as e:
+        logger.error(e)
+        return
+
 
 async def inline_query_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.inline_query
-    # user = query.from_user
+    user = query.from_user
     message = query.query
     results = []
 
     if not message:
+        results.append(inlineQueryMaker("Type your message/text", "Type your message/text"))
+        await query.answer(results)
         return
     
-    try:
-        results.append(InlineQueryResultArticle(
-            str(uuid4()),
-            "Encode: text to base64",
-            InputTextMessageContent(str(BASE64.encode(message))),
-            description=str(BASE64.encode(message))
-        ))
-    except:
-        pass
+    # userID
+    results.append(inlineQueryMaker("Your UserID", f"<code>{user.id}</code>", description=user.id))
+    # base64 encode / decode
+    b64_encode = BASE64.encode(message)
+    b64_decode = BASE64.decode(message)
 
-    try:
-        results.append(InlineQueryResultArticle(
-            str(uuid4()),
-            "Decode: base64 to text",
-            InputTextMessageContent(str(BASE64.decode(message))),
-            description=str(BASE64.decode(message))
-        ))
-    except:
-        pass
+    if b64_encode:
+        results.append(inlineQueryMaker("Base64: Encode (text to base64)", f"<code>{b64_encode}</code>", description=b64_encode))
+    
+    if b64_decode:
+        results.append(inlineQueryMaker("Base64: Decode (base64 to text)", f"<code>{b64_decode}</code>", description=b64_decode))
 
-    if results:
-        await query.answer(results)
+    if not results:
+        return
+    
+    await query.answer(results)
